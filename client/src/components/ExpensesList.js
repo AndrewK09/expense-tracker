2a8e6018440bd66adec9234f6cf7bf88be7461d4
Andrew <Andrewkn09@Gmail.com>
2019-08-16 10:27:40 -0400

    Add pagination

--- client/src/components/ExpensesList.js
+++ client/src/components/ExpensesList.js
@@ -11,10 +11,12 @@ import {
   TableHead,
   TableRow,
   TableSortLabel,
+  TablePagination,
   Toolbar,
   Tooltip,
   Typography,
   IconButton,
+  TableFooter,
 } from '@material-ui/core';
 
 import AddIcon from '@material-ui/icons/add';
@@ -24,6 +26,9 @@ import FormDialog from './menu/FormDialog';
 import * as actions from '../actions/handleExpenses';
 
 const useStyles = makeStyles(theme => ({
+  title: {
+    margin: theme.spacing(0),
+  },
   header: {
     padding: theme.spacing(0),
     justifyContent: 'space-between',
@@ -44,6 +49,9 @@ const ExpensesList = props => {
   const [open, setOpen] = useState(false);
   const [order, setOrder] = useState('asc');
   const [orderBy, setOrderBy] = useState('category');
+  const [page, setPage] = useState(0);
+  const pageRows = 7;
+  const emptyRows = pageRows - Math.min(expenses.length - page * pageRows);
 
   const handleClickForm = e => {
     setOpen(true);
@@ -62,11 +70,15 @@ const ExpensesList = props => {
     props.fetchExpenses(orderBy, order);
   };
 
+  const handleChangePage = (e, nextPage) => {
+    setPage(nextPage);
+  };
+
   return (
     <Box>
       <Toolbar className={classes.header}>
         <Box>
-          <Typography variant="h5" component="h3">
+          <Typography variant="h5" component="h3" className={classes.title}>
             Expenses
           </Typography>
         </Box>
@@ -107,18 +119,38 @@ const ExpensesList = props => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {expenses.map(expense => (
-            <TableRow key={expense.id}>
-              <TableCell>{expense.description}</TableCell>
-              <TableCell>{expense.category}</TableCell>
-              <TableCell>{expense.company}</TableCell>
-              <TableCell align="right">${expense.amount}</TableCell>
-              <TableCell align="right" className={classes.date}>
-                {moment(expense.date).format('MMM-DD-YY')}
-              </TableCell>
+          {expenses
+            .slice(page * pageRows, page * pageRows + pageRows)
+            .map(expense => (
+              <TableRow key={expense.id} hover={true}>
+                <TableCell>{expense.description}</TableCell>
+                <TableCell>{expense.category}</TableCell>
+                <TableCell>{expense.company}</TableCell>
+                <TableCell align="right">${expense.amount}</TableCell>
+                <TableCell align="right" className={classes.date}>
+                  {moment(expense.date).format('M-DD-YY')}
+                </TableCell>
+              </TableRow>
+            ))}
+          {emptyRows > 0 && (
+            <TableRow style={{ height: 45 * emptyRows }}>
+              <TableCell colSpan={5} />
             </TableRow>
-          ))}
+          )}
         </TableBody>
+
+        <TableFooter>
+          <TableRow style={{ borderBottom: '0' }}>
+            <TablePagination
+              style={{ borderBottom: '0' }}
+              count={expenses.length}
+              onChangePage={handleChangePage}
+              page={page}
+              rowsPerPage={pageRows}
+              rowsPerPageOptions={[]}
+            />
+          </TableRow>
+        </TableFooter>
       </Table>
     </Box>
   );
