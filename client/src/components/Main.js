435686e991d94ce6363b7d5d42d3cc69cb574c72
Andrew <Andrewkn09@Gmail.com>
2019-08-17 11:50:39 -0400

    Fix height

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -14,13 +14,13 @@ const useStyles = makeStyles(theme => ({
   paperLeft: {
     margin: '30px 0 0 0px',
     padding: '20px 30px 15px 30px',
-    // height: '80vh',
+    height: '540px',
     overflowX: 'auto',
   },
   paperRight: {
     margin: '30px 0 0 30px',
     padding: '30px 30px 15px 30px',
-    height: '79vh',
+    height: '540px',
   },
 }));
 
